1e22845f28e1222771d3dae5a75503667b448307
Isaac Blender <isblender@ucla.edu>
2024-11-19 19:07:47 -0800

    Proxy to deezer

--- backend/functions/getSongData.js
+++ backend/functions/getSongData.js
@@ -1,12 +1,17 @@
+// /Users/isaacblender/Desktop/songify/backend/functions/getSongData.js
 const axios = require("axios");
 
 const getRecommendedSong = async (query) => {
   try {
-    const response = await axios.get(`https://api.deezer.com/search?q=${encodeURIComponent(query)}`, {
+    // Construct the URL with RapidAPI host and key
+    const response = await axios.get(`https://deezerdevs-deezer.p.rapidapi.com/search`, {
       headers: {
+        "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com", // Replace with your RapidAPI host
+        "x-rapidapi-key": "d16e75deeamsha53546b9562ca7ap18a35djsned2b59b17c2a", // Replace with your RapidAPI key
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36", // Mimic a browser's user agent
         "Accept": "application/json", // Explicitly set the Accept header
       },
+      params: { q: query }, // Pass the query as a parameter
     });
 
     if (response.data && response.data.data.length > 0) {
